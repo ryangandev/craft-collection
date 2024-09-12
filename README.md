08ac10f174882c9c9ac893ea5d3a28299bdd7d38
Ryan Gan <49723210+ryangandev@users.noreply.github.com>
2024-09-12 03:25:29 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+This is a [Next.js](https://nextjs.org/) starter template.
+
+This template includes:
+
+- Most recent version of Next.js
+- TypeScript
+- TailwindCSS
+- Shadcn UI
+- Next Theme
+- Prettier (with Vercel guideline) setup
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import ThemeToggle from '@/components/theme-toggle';
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <ThemeToggle />
+    </main>
+  );
+}
